31aa20e30b790b4c51288a96f6cb4d9fbd2f220a
VinX-To-play <v@lundborgs.de>
2024-11-19 16:16:32 +0100

    fix matplotlib for nixos

--- Assignment1/Lecture 2/hellper_function.py
+++ Assignment1/Lecture 2/hellper_function.py
@@ -1,5 +1,4 @@
-import pickle
-import numpy
+import math
 import matplotlib.pyplot as plt
 from three_sum import *
 import random
@@ -8,6 +7,8 @@ import time
 start = 200
 stop = 701
 step = 50
+
+Savefile = "time.data"
 
 def time_taker(start, stop, step):
     time_data = []
@@ -18,12 +19,30 @@ def time_taker(start, stop, step):
         begin_time = time.time()
         sum_3_brut(lst)
         time_data.append((time.time() - begin_time))
+        print(time_data[len(time_data) - 1])
+
+    saving_func(time_data)
     return time_data
 
-time_data = time_taker(start, stop, step)
+def saving_func(time_data):
+    with open(Savefile, "wt") as data:
+        for i in range(len(time_data)):
+            data.write(f"{str(time_data[i])} \n")
+
+
+def read_funk(Savefile):
+    with open("time.data", "r") as file:
+        data = file.read()
+        return data.split()
+
+
+
+time_data = read_funk(Savefile)
+#time_data = time_taker(start, stop, step)
 print(time_data)
+saving_func(time_data)
 
-with open('save.pkl', 'wb') as save:
-    pickle.dump(time_data, save, -1)
 
-plt.plot((range(start,stop + 1, step)), time_data)
+y_axis = range(start, stop, step)
+plt.plot(time_data, y_axis)
+plt.show()
---
+++ Assignment1/Lecture 2/test.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+
+
+
+
+
+
+xpoints = (1,2,2,3,5)
+ypoints = (1,2,3,4,5)
+
+plt.plot(xpoints, ypoints)
+plt.show()
