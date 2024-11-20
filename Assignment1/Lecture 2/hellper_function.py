32cdeabc87ff1e1083a5747c042a22ef0e1e9a68
VinX-To-play <v@lundborgs.de>
2024-11-20 14:05:56 +0100

    log test workt + fix of testing

--- Assignment1/Lecture 2/hellper_function.py
+++ Assignment1/Lecture 2/hellper_function.py
@@ -14,8 +14,8 @@ def time_taker(start, stop, step):
     time_data = []
     for i in range(start, stop, step):
         lst = []
-        for _ in range(start, i):
-            lst.append(random.randint(-10 * i, 10 * i))
+        for _ in range(i):
+            lst.append(random.randint(-100, 100))
         begin_time = time.time()
         sum_3_brut(lst)
         time_data.append((time.time() - begin_time))
@@ -32,17 +32,36 @@ def saving_func(time_data):
 
 def read_funk(Savefile):
     with open("time.data", "r") as file:
+        time_data = []
         data = file.read()
-        return data.split()
+        for i in data.split():
+            time_data.append(float(i))
+        return time_data
+
+def lin_graph(start, stop, step, time_data):
+    x_axis = [*range(start, stop, step)]
+    plt.xlabel(f"iterration from {start} to {stop} in {step} distanc")
+    plt.ylabel("time")
+    plt.plot(x_axis, time_data)
+    plt.show()
+
+
+def log_graph(start, stop, step, time_data):
+    x_axis = [*range(start, stop, step)]
+    plt.xlabel(f"iterration from {start} to {stop} in {step} distanc in log")
+    plt.ylabel("time in log")
+    logX = [math.log(x) for x in x_axis]
+    logY = [math.log(y) for y in time_data]
+    for i in range(len(time_data)):
+        print(logY[i], time_data[i])
+    plt.plot(logX, logY)
+    plt.show()
 
 
 
-time_data = read_funk(Savefile)
-#time_data = time_taker(start, stop, step)
+#time_data = read_funk(Savefile)
+time_data = time_taker(start, stop, step)
 print(time_data)
 saving_func(time_data)
-
-
-y_axis = range(start, stop, step)
-plt.plot(time_data, y_axis)
-plt.show()
+lin_graph(start, stop, step,time_data)
+log_graph(start, stop, step,time_data)
